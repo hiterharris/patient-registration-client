d5228a3af2789eac3b664e9ad38de452ec996e7a
Hiter Harris <hiterharris@comcast.net>
2022-07-05 20:50:48 -0400

    update styling

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,44 +3,35 @@ import '../App.css';
 const RegistrationForm = ({ newUser, setNewUser, submitUser }) => {
     return (
         <div className="RegistrationForm">
-            <h1>New Patient Registration</h1>
+            <h1>Register</h1>
             <form>
                 <div className="input-field">
-                    <label>Name:</label>
                     <input type="text" placeholder="Name" name="name" onChange={(event) => setNewUser({ ...newUser, name: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>Date of Birth:</label>
                     <input type="text" placeholder="Date of Birth" name="dateOfBirth" onChange={(event) => setNewUser({ ...newUser, dateOfBirth: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>Phone:</label>
                     <input type="text" placeholder="Phone Number" name="phone" onChange={(event) => setNewUser({ ...newUser, phone: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>Email:</label>
                     <input type="text" placeholder="Email Address" name="email" onChange={(event) => setNewUser({ ...newUser, email: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>Address:</label>
                     <input type="text" placeholder="Address" name="addressLine1" onChange={(event) => setNewUser({ ...newUser, addressLine1: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>City:</label>
                     <input type="text" placeholder="City" name="city" onChange={(event) => setNewUser({ ...newUser, city: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>State:</label>
                     <input type="text" placeholder="State" name="state" onChange={(event) => setNewUser({ ...newUser, state: event.target.value})} />
                 </div>
                 <div className="input-field">
-                    <label>Zip Code:</label>
                     <input type="text" placeholder="Zip Code" name="zip" onChange={(event) => setNewUser({ ...newUser, zip: event.target.value})} />
                 </div>
-                <div className="input-field" />
-                <div className="input-field" />
             </form>
             <button type="submit" className="submit-button" onClick={() => submitUser()}>Submit</button>
+            <p className="account">Already have an account? <a href="/" className="login"> Login</a></p>
         </div>
     );
 }
