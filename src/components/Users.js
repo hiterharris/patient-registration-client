ec481fdca46d1d52ccdaeeef728177d328a104e3
Hiter Harris <hiterharris@comcast.net>
2022-07-08 17:03:49 -0400

    update with main

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,17 @@
 import useAuth from '../hooks/useAuth';
 import '../App.css'
 
-const Users = ({ users }) => {
+const Users = ({ users, deleteUser }) => {
     const { submitLogout } = useAuth();
+
+    const handleDelete = (user) => {
+        deleteUser(user.userId);
+        setTimeout(() => window.location.reload(true), 1000);
+    }
+
     return (
         <div className="Users">
-            <h1 className="users-title">Patients</h1>
+            <h1 className="users-title">Users</h1>
             <div className="users-list">
                 {users.map((user, index) => {
                     return (
@@ -24,6 +30,7 @@ const Users = ({ users }) => {
                             <p><span>Date Created: </span>{user.registrationTime.slice(0, -29)}</p>
                             <span>Photo ID:</span><br />
                             <img className="selected-file" src={`https://patient-registration.s3.amazonaws.com/${user?.photoId}`} alt="selected file" />
+                            <h4 className="delete" onClick={() => handleDelete(user)}>Delete</h4>
                         </div>
                     );
                 })}
